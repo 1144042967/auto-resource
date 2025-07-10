66d0a108301879c1ae44a29c4dd93fba5c13337a
JRZ <1017752020@qq.com>
2025-07-10 22:27:02 +0800

    支持 1.20.4

--- src/main/java/cn/sd/jrz/autoresource/Config.java
+++ src/main/java/cn/sd/jrz/autoresource/Config.java
@@ -1,7 +1,6 @@
 package cn.sd.jrz.autoresource;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -317,7 +316,7 @@ public class Config {
         SERVER_CONFIG = SERVER_BUILDER.build();
     }
 
-    public static void init( ) {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_CONFIG);
+    public static void init(FMLJavaModLoadingContext context) {
+        context.registerConfig(ModConfig.Type.SERVER, SERVER_CONFIG);
     }
 }
