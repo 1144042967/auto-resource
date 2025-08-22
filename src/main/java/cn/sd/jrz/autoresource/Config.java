f1a53b742a8a2721e037619968e75d8682823ad6
江荣展 <jiangrzh@eastsoft.com.cn>
2025-08-22 11:50:36 +0800

    添加额外的右键功能
    - 流体机主手/副手拿空桶右击时，将取出一桶液体
    - 方块机主手为空/主手拿相同方块时，将取出一个方块

--- src/main/java/cn/sd/jrz/autoresource/Config.java
+++ src/main/java/cn/sd/jrz/autoresource/Config.java
@@ -3,7 +3,6 @@ package cn.sd.jrz.autoresource;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class Config {
 
@@ -317,7 +316,7 @@ public class Config {
         SERVER_CONFIG = SERVER_BUILDER.build();
     }
 
-    public static void init( ) {
+    public static void init() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_CONFIG);
     }
 }
